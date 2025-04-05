9028b5d7911091cf0ce2d11c05bb573a4dcfd504
Sachin Gothwal <sachingothwal@Sachins-MacBook-Air.local>
2025-04-05 15:01:02 +0530

    error message handling

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -13,11 +13,9 @@ const Register = () => {
 		import.meta.url
 	).toString();
 
-	const [isErrorOnEmail1, setIsErrorOnEmail1] = useState(false);
-	const [isErrorOnPassword1, setIsErrorOnPassword1] = useState(false);
-	const [isErrorOnEmail2, setIsErrorOnEmail2] = useState(false);
-	const [isErrorOnPassword2, setIsErrorOnPassword2] = useState(false);
-	// const [message, setMessage] = useState("");
+	const [errorMessage1, setErrorMessage1] = useState("");
+	const [errorMessage2, setErrorMessage2] = useState("");
+	const [message, setMessage] = useState("");
 
 	const email1: string | RefObject<HTMLInputElement> = useRef(null as unknown as HTMLInputElement);
 	const password1: string | RefObject<HTMLInputElement> = useRef(null as unknown as HTMLInputElement);
@@ -28,9 +26,10 @@ const Register = () => {
 		try {
 			await createUserWithEmailAndPassword(auth, email, password).then((value: UserCredential) => {
 				// console.log(value);
+				setMessage("");
 			}).catch((error) => {
 				if (error.code === 'auth/email-already-in-use') {
-					// setMessage("User Exists!!!");
+					setMessage("User Exists!!!");
 				}
 			})
 
@@ -41,35 +40,31 @@ const Register = () => {
 
 	const registerFirst = async (): Promise<void> => {
 		if (!emailAndPhoneNumberValidator(email1.current.value)) {
-			setIsErrorOnEmail1(true);
-			// setMessage("Invalid email address");
+			setErrorMessage1("Invalid email address");
 			return;
 		}
 		if (!passwordValidator(password1.current.value)) {
-			setIsErrorOnPassword1(true);
-			// setMessage("Invalid password");
+			setErrorMessage1("Invalid password");
 			return;
 		}
 
-		setIsErrorOnEmail1(false);
-		setIsErrorOnPassword1(false);
+		setErrorMessage1("");
+		setMessage("");
 		register(email1.current.value, password1.current.value);
 	}
 
 	const registerSecond = (): void => {
 		if (!emailAndPhoneNumberValidator(email2.current.value)) {
-			setIsErrorOnEmail2(true);
-			// setMessage("Invalid email address")
+			setErrorMessage2("Invalid email address");
 			return;
 		}
 		if (!passwordValidator(password2.current.value)) {
-			setIsErrorOnPassword2(true);
-			// setMessage("Invalid password");
+			setErrorMessage2("Invalid password");
 			return;
 		}
 
-		setIsErrorOnEmail2(false);
-		setIsErrorOnPassword2(false);
+		setErrorMessage2("");
+		setMessage("");
 		register(email2.current.value, password2.current.value);
 	}
 
@@ -95,15 +90,16 @@ const Register = () => {
 
 					<form onSubmit={(e) => e.preventDefault()} className="flex text-white m-6 justify-center items-center">
 						<div className="group relative flex">
-							<input required type="email" id="email-first" className={"bg-neutral-500 mr-2 rounded-full w-80 pt-4 px-5 peer h-16 outline-offset-2 " + (isErrorOnEmail1 ? 'outline-2 outline-red-700' : 'focus:outline-2 outline-white')} ref={email1} />
+							<input required type="email" id="email-first" className={"bg-neutral-500 mr-2 rounded-full w-80 pt-4 px-5 peer h-16 outline-offset-2 " + ((errorMessage1 === "Invalid email address") ? 'outline-2 outline-red-700' : 'focus:outline-2 outline-white')} ref={email1} />
 							<label htmlFor="email-first" className="transform transition-all absolute pl-5 top-0 h-full flex items-center text-sm group-focus-within:text-xs group-focus-within:h-1/2 group-focus-within:-translate-y-0.5 peer-valid:text-xs peer-valid:h-1/2 peer-valid:-translate-y-0.5">Email address</label>
 
-							<input required type="password" id="password-first" className={"bg-neutral-500 mr-2 rounded-full w-80 pt-4 px-5 peer h-16 outline-offset-2 " + (isErrorOnPassword1 ? 'outline-2 outline-red-700' : 'focus:outline-2 outline-white')} ref={password1} />
+							<input required type="password" id="password-first" className={"bg-neutral-500 mr-2 rounded-full w-80 pt-4 px-5 peer h-16 outline-offset-2 " + ((errorMessage1 === "Invalid password") ? 'outline-2 outline-red-700' : 'focus:outline-2 outline-white')} ref={password1} />
 							<label htmlFor="password-first" className="transform transition-all absolute pl-5 top-0 left-82 h-full flex items-center text-sm group-focus-within:text-xs group-focus-within:h-1/2 group-focus-within:-translate-y-0.5 peer-valid:text-xs peer-valid:h-1/2 peer-valid:-translate-y-0.5">Password</label>
 
 							<button className="bg-red-700 focus:outline-2 outline-white outline-offset-2 px-12 py-4 rounded-full text-2xl font-semibold" onClick={registerFirst}>Get Started</button>
 						</div>
 					</form>
+					<div className={(errorMessage1.length !== 0 || message.length !== 0) ? "text-white w-4xl p-2 m-auto rounded-full text-center bg-red-500" : "hidden"}>{errorMessage1 || message}</div>
 				</div>
 			</div>
 
@@ -117,17 +113,18 @@ const Register = () => {
 					Ready to watch? Enter your email to create or restart your
 					membership.
 				</h4>
-				<form onSubmit={(e) => e.preventDefault()} className="flex text-white justify-center items-center">
+				<form onSubmit={(e) => e.preventDefault()} className="flex m-6 text-white justify-center items-center">
 					<div className="group relative flex">
-						<input required type="email" id="email-second" className={"bg-neutral-500 mr-2 rounded-full w-80 pt-4 px-5 h-16 peer outline-offset-2 " + (isErrorOnEmail2 ? 'outline-2 outline-red-700' : 'focus:outline-2 outline-white')} ref={email2} />
+						<input required type="email" id="email-second" className={"bg-neutral-500 mr-2 rounded-full w-80 pt-4 px-5 h-16 peer outline-offset-2 " + ((errorMessage2 === "Invalid email address") ? 'outline-2 outline-red-700' : 'focus:outline-2 outline-white')} ref={email2} />
 						<label htmlFor="email-second" className="transform transition-all absolute pl-5 top-0 h-full flex items-center text-sm group-focus-within:text-xs group-focus-within:h-1/2 group-focus-within:-translate-y-0.5 peer-valid:text-xs peer-valid:h-1/2 peer-valid:-translate-y-0.5">Email address</label>
 
-						<input required type="password" id="password-second" className={"bg-neutral-500 mr-2 rounded-full w-80 pt-4 px-5 h-16 peer outline-offset-2 " + (isErrorOnPassword2 ? 'outline-2 outline-red-700' : 'focus:outline-2 outline-white')} ref={password2} />
+						<input required type="password" id="password-second" className={"bg-neutral-500 mr-2 rounded-full w-80 pt-4 px-5 h-16 peer outline-offset-2 " + ((errorMessage2 === "Invalid password") ? 'outline-2 outline-red-700' : 'focus:outline-2 outline-white')} ref={password2} />
 						<label htmlFor="email-second" className="transform transition-all absolute pl-5 top-0 left-82 h-full flex items-center text-sm group-focus-within:text-xs group-focus-within:h-1/2 group-focus-within:-translate-y-0.5 peer-valid:text-xs peer-valid:h-1/2 peer-valid:-translate-y-0.5">Password</label>
 
 						<button className="bg-red-700 focus:outline-2 outline-white outline-offset-2 px-12 py-4 rounded-full text-2xl font-semibold" onClick={registerSecond}>Get Started</button>
 					</div>
 				</form>
+				<div className={(errorMessage2.length !== 0 || message.length !== 0) ? "text-white w-4xl p-2 m-auto rounded-full text-center bg-red-500" : "hidden"}>{errorMessage2 || message}</div>
 			</div>
 
 			<Footer />
--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -11,32 +11,33 @@ const SignIn = () => {
         import.meta.url
     ).toString();
 
-    const [isEmailOrPhoneError, setIsEmailOrPhoneError] = useState(false);
-    const [isPasswordError, setIsPasswordError] = useState(false);
+    const [errorMessage, setErrorMessage] = useState("");
+    const [message, setMessage] = useState("");
     const email: string | RefObject<HTMLInputElement> = useRef(null as unknown as HTMLInputElement);
     const password: string | RefObject<HTMLInputElement> = useRef(null as unknown as HTMLInputElement);
 
     const signIn = async () => {
         if(!emailAndPhoneNumberValidator(email.current.value)) {
-            setIsEmailOrPhoneError(true);
+            setErrorMessage("Invalid email address");
+            console.log(message);
             return;
         }
         if(!passwordValidator(password.current.value)) {
-            setIsPasswordError(true);
+            setErrorMessage("Invalid password");
+            console.log(message);
             return;
         }
         
-        setIsPasswordError(false);
-        setIsEmailOrPhoneError(false);
-        try {
-            await signInWithEmailAndPassword(auth, email.current.value, password.current.value).then((value: UserCredential) => {
-                // console.log(value);
-            }).catch((error) => {
-                // console.log(error);
-            })
-        } catch (error) {
-            // console.log(error);
-        }
+        setMessage("");
+        setErrorMessage("");
+        console.log(message);
+
+        await signInWithEmailAndPassword(auth, email.current.value, password.current.value).then((value: UserCredential) => {
+            // console.log(value);
+        }).catch((error) => {
+            if (error.code === 'auth/invalid-credential') setMessage("Invalid credentials!");
+            console.log(error);
+        })
     }
 
     return (
@@ -45,19 +46,21 @@ const SignIn = () => {
                 <img className="w-45" src={logo} alt="logo" />
                 <div className="text-white bg-black opacity-80 shadow-2xl w-2/6 p-12 m-auto rounded-sm">
                     <h1 className="text-4xl font-bold my-6">Sign In</h1>
-                    <form onSubmit={(e) => e.preventDefault()} className='mb-12'>
+                    <form onSubmit={(e) => e.preventDefault()}>
                         <div className='group relative mb-3'>
-                            <input required type="tel | email" id="emailOrMobile" className={'px-5 pt-4 h-16 w-full border-2 rounded-sm outline-0 peer ' + (isEmailOrPhoneError ? 'border-red-700' : 'border-neutral-400 focus:border-white') } ref={email} />
-                            <label htmlFor="emailOrMobile" className='transform transition-all absolute pl-5 top-0 h-full flex items-center text-sm group-focus-within:text-xs group-focus-within:h-1/2 group-focus-within:-translate-y-0.5 group-focus-within:pt-0.5 peer-valid:text-xs peer-valid:h-1/2 peer-valid:-translate-y-0.5 peer-valid:pt-0.5'>Email or mobile number</label>
+                            <input required type="email" id='email' className={'px-5 pt-4 h-16 w-full border-2 rounded-sm outline-0 peer ' + (errorMessage.length !== 0 ? 'border-red-700' : 'border-neutral-400 focus:border-white') } ref={email} />
+                            <label htmlFor="email" className='transform transition-all absolute pl-5 top-0 h-full flex items-center text-sm group-focus-within:text-xs group-focus-within:h-1/2 group-focus-within:-translate-y-0.5 group-focus-within:pt-0.5 peer-valid:text-xs peer-valid:h-1/2 peer-valid:-translate-y-0.5 peer-valid:pt-0.5'>Email address</label>
                         </div>
                         <div className='group relative mb-3'>
-                            <input required type="password" id="password" className={'px-5 pt-4 h-16 w-full border-2 rounded-sm outline-0 peer ' + (isPasswordError ? 'border-red-700' : 'border-neutral-400 focus:border-white' )} ref={password} />
+                            <input required type="password" className={'px-5 pt-4 h-16 w-full border-2 rounded-sm outline-0 peer ' + (errorMessage.length !== 0 ? 'border-red-700' : 'border-neutral-400 focus:border-white' )} ref={password} />
                             <label htmlFor="password" className='transform transition-all absolute pl-5 top-0 h-full flex items-center text-sm group-focus-within:text-xs group-focus-within:h-1/2 group-focus-within:-translate-y-0.5 group-focus-within:pt-0.5 peer-valid:text-xs peer-valid:h-1/2 peer-valid:-translate-y-0.5 peer-valid:pt-0.5'>Password</label>
                         </div>
                         <button className='p-5 h-13 w-full bg-red-700 flex items-center justify-center rounded-sm shadow-xl focus:outline-2 outline-white outline-offset-2' onClick={signIn}>Sign In</button>
                     </form>
 
-                    <p>New to Netflix? <NavLink to="/" tabIndex={0} className={ isActive => 'font-bold focus:outline-2 outline-white outline-offset-2 hover:underline hover:underline-offset-2'}>Sign up now.</NavLink></p>
+                    <div className={(errorMessage.length !== 0 || message.length !== 0) ? "text-white p-2 m-auto mt-4 rounded-full text-center bg-red-500" : "hidden"}>{errorMessage || message}</div>
+
+                    <p className='mt-9'>New to Netflix? <NavLink to="/" tabIndex={0} className={ isActive => 'font-bold focus:outline-2 outline-white outline-offset-2 hover:underline hover:underline-offset-2'}>Sign up now.</NavLink></p>
                 </div>
             </div>
             <div className="w-7/10 my-9 m-auto">
